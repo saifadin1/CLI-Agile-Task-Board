4ed79837e75c8bb835aa2cbd29d21d9d295dda22
Saif Medhat <seifmedhatfarouk@gmail.com>
2025-12-29 19:22:20 +0200

    feat: add TaskDetails

---
+++ include/ui/TaskDetails.h
@@ -0,0 +1,13 @@
+#pragma once
+
+using namespace std;
+
+
+class TaskDetails {
+    public:
+        TaskDetails();
+        ~TaskDetails();
+
+        void displayTaskInfo(int taskId) const;
+        void backButton() const;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
 #include "DatabaseSeeder.h"
+#include "TaskDetails.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "CLI Agile Task Board" << endl;
-    cout << "====================" << endl;
-
-    DatabaseSeeder seeder;
-
-    if (seeder.initializeDatabase())
-    {
-        cout << endl << "✅ Database setup complete!" << endl;
-    }
-    else
-    {
-        cout << endl << "❌ Database setup failed!" << endl;
-        return 1;
-    }
 
     return 0;
 }
